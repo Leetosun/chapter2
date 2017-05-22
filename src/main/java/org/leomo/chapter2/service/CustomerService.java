a52af13f82f7a1fb3dd383ad3f54de19c0a7bc89
Leetosun <leetosun@foxmail.com>
2017-05-22 09:19:57 +0800

    封装JDBC连接

--- src/main/java/org/leomo/chapter2/service/CustomerService.java
+++ src/main/java/org/leomo/chapter2/service/CustomerService.java
@@ -1,5 +1,6 @@
 package org.leomo.chapter2.service;
 
+import org.leomo.chapter2.helper.DatabaseHelper;
 import org.leomo.chapter2.model.Customer;
 import org.leomo.chapter2.util.PropsUtil;
 import org.slf4j.Logger;
@@ -17,27 +18,6 @@ public class CustomerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
-    private static final String DRIVER;
-
-    private static final String URL;
-
-    private static final String USERNAME;
-
-    private static final String PASSWORD;
-
-    static {
-        Properties properties = PropsUtil.loadProps("config.properties");
-        DRIVER = properties.getProperty("jdbc.driver");
-        URL = properties.getProperty("jdbc.url");
-        USERNAME = properties.getProperty("jdbc.username");
-        PASSWORD = properties.getProperty("jdbc.password");
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            LOGGER.error("can not load jdbc driver", e);
-        }
-    }
-
     public List<Customer> getCustomerList(String keyword) {
         //TODO
         return null;
@@ -49,7 +29,7 @@ public class CustomerService {
         try {
             List<Customer> customers = new ArrayList<>();
             String sql = "SELECT * FROM customer";
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DatabaseHelper.getConnection();
             PreparedStatement stmt = connection.prepareStatement(sql);
             ResultSet resultSet = stmt.executeQuery();
             while (resultSet.next()) {
@@ -66,13 +46,7 @@ public class CustomerService {
         } catch (SQLException e) {
             LOGGER.error("execute sql failure", e);
         } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    LOGGER.error("close connection failure", e);
-                }
-            }
+            DatabaseHelper.closeConnection(connection);
         }
         return null;
     }
